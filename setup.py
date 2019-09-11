4ecddb76be84f47e410648f603150d2fb2e161a6
karabanb <bkarabands@gmail.com>
2019-09-11 20:22:13 +0200

    setup.py was configured

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = 'bkmisc'
+REQUIREMENTS_PATH = 'requirements.txt'
+README_PATH = 'README.md'
+VERSION = '0.0.1'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    url='https://github.com/karabanb/pybkmisc',
+    author='Bartłomiej Karaban',
+    author_email='bkarabands@gmail.com'
+)
