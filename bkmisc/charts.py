ef2b33ec9e94a2cd369a0901dc7dacf9405322b9
karabanb <bkarabands@gmail.com>
2019-09-13 23:55:08 +0200

    class PredictionsTable fixed

--- bkmisc/charts.py
+++ bkmisc/charts.py
@@ -4,14 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-class PredictionTable:
-    def __init__(self, df):
-        self.df = df
+class PredictionsTable():
+    def __init__(self, y_true, pred_probas):
+        self.y_true = y_true
+        self.pred_probas = pred_probas
 
-    @classmethod
-    def classmethod(cls):
-        return cls
+    def __repr__(self):
+        df = np.cumsum(self.y_true)
+        return repr(df)
 
+
+
+
+'''
 raw_data_pp = raw_data.loc[raw_data.score_pp > 0, ['score_pp', 'YprePierwsze']]
 raw_data_kp = raw_data.loc[raw_data.score_kp > 0, ['score_kp', 'YpreKolejne']]
 # sort values
@@ -29,3 +34,4 @@ sorted_df['KS'] = np.abs(sorted_df.pct_target - sorted_df.pct_nontarget) * 100
 plt.plot(sorted_df.score_pp, sorted_df.pct_target)
 plt.plot(sorted_df.score_pp, sorted_df.pct_nontarget)
 plt.show()
+'''
