d70206f7cbe21ac806f33fa5fa47c0f81234232d
karabanb <bkarabands@gmail.com>
2019-09-14 14:30:53 +0200

    shortest version of start_project()

--- bkmisc/tools.py
+++ bkmisc/tools.py
@@ -4,5 +4,4 @@ import os
 
 def start_project():
     for folder in ['data', 'doc', 'results', 'scripts', 'tmp']:
-        os.mkdir(os.path.join(os.getcwd(), folder))
-
+        os.mkdir('./' + folder)
